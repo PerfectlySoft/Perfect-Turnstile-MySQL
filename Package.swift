b99972fa3e650943af2403a6d88086976e320f4a
Nawar Nory <nawar.a.nouri@gmail.com>
2017-01-18 19:53:26 -0400

    adding a MYSQL-StORM that supports joins

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PerfectTurnstileMySQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/nawar/MySQL-StORM.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
