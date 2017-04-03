00fb04ec2f2e443c379fe0b70fd0c5824377d2be
Jonathan Guthrie <jono@perfect.org>
2017-04-03 11:35:06 -0400

    fixing minor versions

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ let package = Package(
     name: "PerfectTurnstileMySQL",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
         .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
+        .Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 2),
     ]
 )
