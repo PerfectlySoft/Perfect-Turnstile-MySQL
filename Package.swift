135e56b100e28ededd7cde6cfa4a975e92b660cf
Jonathan Guthrie <jono@perfect.org>
2017-10-11 17:11:29 -0400

    update for perfect 3, swift 4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 17:28:01 +0000
+// Date: 2017-10-11 20:57:35 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectTurnstileMySQL",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
-        .Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 2),
-    ]
+	name: "PerfectTurnstileMySQL",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
+		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 3),
+	]
 )
